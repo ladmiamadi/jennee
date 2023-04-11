dd0d7dcd5705a67320f1ed6d591920d81ac83d0b
AnoneWk <anonewk0@gmail.com>
2023-04-11 14:04:53 +0200

    feat: added dza in modal jsx

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -9,6 +9,7 @@ import ModalExit from './modalExit'
 const ModalEvent = ({ open, handleClose, formData, handleChange, handleSubmit, errors, step, handleNext, handlePrev }) => {
     return (
         <Box>
+            dza
             {step === 1 && (
                 <Modal
                     className="modal"
