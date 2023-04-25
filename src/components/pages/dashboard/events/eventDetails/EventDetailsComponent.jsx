391558a5f406f8cae7ba0bea8ee0f46a32885d45
anonewk0 <anonewk0@gmail.com>
2023-04-25 10:54:49 +0200

    feat: begin event detail page header

--- src/components/pages/dashboard/events/eventDetails/EventDetailsComponent.jsx
+++ src/components/pages/dashboard/events/eventDetails/EventDetailsComponent.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import HeaderEventDetails from '@common/headerPageContent/HeaderEventsDetails'
 
 const EventDetailsComponent = () => {
-    return <div>test</div>
+    return (
+        <Box className={'events-details-container'}>
+            <HeaderEventDetails />
+        </Box>
+    )
 }
 
 export default EventDetailsComponent
